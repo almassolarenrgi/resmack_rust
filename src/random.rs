b28a840ed11828344e2bdeeb782c08279fe4b897
James Johnson <d0c.s4vage@gmail.com>
2020-05-22 07:06:24 -0700

    Add documentation for Rand

--- src/random.rs
+++ src/random.rs
@@ -4,6 +4,20 @@ use rand::distributions::uniform::SampleUniform;
 use rand::Rng;
 use rand_pcg;
 
+/// The rand struct exposes the `rand_int` function for number generation.
+///
+/// ```
+/// use resmack::random::Rand;
+///
+/// let seed = 1337;
+/// // must be mutable!
+/// let mut rand = Rand::new(seed);
+/// let res = rand.rand_int(-5, 5);
+///
+/// for _ in (0..100) {
+///     assert_eq!(-5 <= res && res < 5, true);
+/// }
+/// ```
 pub struct Rand {
     rng: rand_pcg::Pcg64Mcg,
 }
@@ -15,6 +29,7 @@ impl Rand {
         }
     }
 
+    /// Generates a new value in the range `[min, max)`
     pub fn rand_int<T: SampleUniform, B1, B2>(&mut self, min: B1, max: B2) -> T
     where
         B1: SampleBorrow<T> + Sized,
